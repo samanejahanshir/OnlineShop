776fbe9289c0e497a54eab86d61ab9296e8a98c1
unknown <jahanshir.sm92@gmail.com>
2021-10-17 09:24:37 -0700

    add OrderDao class

--- src/models/Orders.java
+++ src/models/Orders.java
@@ -8,8 +8,60 @@ public class Orders {
     private int productId;
     private String type;
     private Date date;
-    private BuyStatus status;
+    private String status;
 
+    public Orders(int userId, int productId, String type, String status) {
+        this.userId = userId;
+        this.productId = productId;
+        this.type = type;
+        this.status = status;
+    }
 
+    public int getId() {
+        return id;
+    }
 
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public int getProductId() {
+        return productId;
+    }
+
+    public void setProductId(int productId) {
+        this.productId = productId;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
 }

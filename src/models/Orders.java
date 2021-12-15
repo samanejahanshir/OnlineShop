d14110d8755bbb062e663047de74b3be9fffced0
unknown <jahanshir.sm92@gmail.com>
2021-12-15 18:05:33 +0330

    add @Entity in Order class

--- src/models/Orders.java
+++ src/models/Orders.java
@@ -1,14 +1,33 @@
 package models;
 
+import javax.persistence.*;
 import java.time.LocalDateTime;
 import java.util.Date;
-
+@Entity
 public class Orders {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+    @Transient
     private int userId;
+    @Transient
     private int productId;
     private LocalDateTime date;
     private String status;
+    @OneToOne(cascade = CascadeType.ALL)
+    private User user;
+
+    public Orders() {
+
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
 
     public Orders(int userId, int productId, String status) {
         this.userId = userId;
